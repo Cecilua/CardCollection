5e2f14cc5d9b9ce5e4b1229d2081f952160d4086
Cecilua <65154517+Cecilua@users.noreply.github.com>
2023-05-10 15:11:48 +1200

    addCard -- added double validation

--- GUI.java
+++ GUI.java
@@ -19,15 +19,21 @@ public class GUI
         cards = new Cards();
         UI.initialise();
         UI.addButton("add a card", this::addCard);
+        UI.addButton("quit", UI::quit);
     }
     
     /**
      *  add a card to the collection 
      */
     public void addCard() {
+        // set constants 
+        final double MIN_VALUE = 0; 
+        final double MAX_VALUE = 10000000;
+        
         // ask the user for details 
         String nm = UI.askString("name: ").trim();
-        double val = UI.askDouble("market value: ");
+        
+        double val = isValidDouble("market value: ", MIN_VALUE, MAX_VALUE);
         
         //add an image to display in GUI
         String img = UIFileChooser.open("choose image file: ");
@@ -35,4 +41,22 @@ public class GUI
         // add the card to the collection
         cards.addCard(nm, val, img); 
     }
+    
+    /**
+     *  takes an question, a minimum and a maximum number
+     *  @return double if valid 
+     */
+    public double isValidDouble(String question, double min, double max) {
+        double number = min; // innitialise the number as the minimum 
+        while (number <= min || number >= max) {
+            number = UI.askDouble(question); 
+            if (number <= min) {
+                UI.println("number must be greater than " + min);
+            } else if (number >= max) {
+                UI.println("number must be less than " + max); 
+            }
+        }
+        return number; 
+    }
+    
 }
